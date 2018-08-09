2f4e2f9c5d9647d9bd1b2dd676ee61e727213288
vic <vdmings@gmail.com>
2018-08-09 21:53:56 +0200

    Added line to start demo script

--- start-demo.sh
+++ start-demo.sh
@@ -7,3 +7,5 @@ kubectl apply -f kubernetes/gijs/ingress-service.yaml -n gijs-space
 kubectl apply -f kubernetes/maria/ingress-service.yaml -n  maria-space
 kubectl apply -f kubernetes/martijn/ingress-service.yaml -n martijn-space
 kubectl apply -f kubernetes/rob/ingress-service.yaml  -n rob-space
+
+watch -n 0.2 -d 'kubectl get po --all-namespaces -l ttech-app -L ttech-app  -o wide'
